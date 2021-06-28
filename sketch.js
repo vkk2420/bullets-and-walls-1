d260e057d8650c2e5acda8f89c896716a59ac585
Arth4567 <82848451+Arth4567@users.noreply.github.com>
2021-06-28 16:26:32 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var bullet,wall
+var speed,weight,thickness
+
+
+
+function setup() {
+  createCanvas(1600,400);
+
+speed=random(55,90)
+weight=random(400,1500)
+
+car=createSprite(50,200,50,30)
+bullet.shapeColor(255)
+car.velocityX = speed;
+
+wall=createSprite(1200,200,thickness,height/2)
+wall.shapeColor(80,80,80)
+
+thickness=random(22,83)
+speed=random(233,321)
+weight=random(50,52)
+}
+
+function draw() {
+  background(0);  
+
+if(hasCollided(bullet,wall)){
+
+  bullet.velocityX=0;
+  var damage=0.5 * weight * speedd* speed/(thickness *thickness *thickness)
+
+  if(damage>10){
+    wall.shapeColour=color(255,0,0)
+  }
+
+  if(damage<10){
+    wall.shapeColor=color(0,255,0)
+  }
+
+}
+
+
+
+  drawSprites();
+}
+
+
+function hasCollided(bullet,wall){
+  
+ bulletRightEdge=bullet.x+bullet.width;
+  wallLeftEdge=wall.x
+ if(bulletRightEdge>=wallleftEdge){
+   return true
+ }
+ return false
+
+}
+
